d52494855e034db1eebe8f520bf8e7cfd1526d86
Ilya Ashchepkov <koctep@gmail.com>
2017-04-23 04:40:38 +0000

    Text formatting and small fixes

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-events=[
-{name: "Искра", 
-id:235
+events = [{
+  name: "Искра",
+  id:235
 },
-{name: "Король говорит",
-id:229
+{
+  name: "Король говорит",
+  id:229
 },
-{name: "Красное на черном",
-id:212
-},
-]
+{
+  name: "Красное на черном",
+  id:212
+}];
 
-for (var i=0; i<events.length; i++)
-{show(events[i]);}
+for (var i = 0; i < events.length; i++) {
+  show(events[i]);
+}
 
 function show(game) {
-	var gameList=jQuery("#games");
-	var gameLink="<li><a href=\""+game.id+"\">"+game.name+"</a></li>";
-gameList.append(gameLink);}
+	var gameList = jQuery("#games");
+	var gameLink = "<li><a href=\"" + game.id + "\">" + game.name + "</a></li>";
+  gameList.append(gameLink);
+}
